c8481401027a2a92479517aa53a37945b4c4fbb2
kseniya <efremovaks@gmail.com>
2020-12-28 14:00:19 +0300

    Автоматизируем сборку личного проекта

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,53 @@ const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
+const rename = require("gulp-rename");
+const del = require("del");
+const cssmin = require("gulp-csso");
+const htmlmin = require("gulp-htmlmin");
+const jsmin = require("gulp-uglify");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+
+
+// Html mini
+
+const htmlmini = () => {
+  return gulp.src("source/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("build"));
+}
+
+// JS mini
+
+const jsmimi = () => {
+  return gulp.src("source/js/menu.js")
+    .pipe(jsmin())
+    .pipe(rename("menu.min.js"))
+    .pipe(gulp.dest("build/js"))
+    .pipe(sync.stream());
+}
+
+exports.jsmini = jsmimi;
 
 // Styles
+
+// const styles = () => {
+//   return gulp.src("source/less/style.less")
+//     .pipe(plumber())
+//     .pipe(sourcemap.init())
+//     .pipe(less())
+//     .pipe(postcss([
+//       autoprefixer(),
+//       csso()
+//     ]))
+//     .pipe(rename("style.min.css"))
+//     .pipe(sourcemap.write("."))
+//     .pipe(gulp.dest("build/css"))
+//     .pipe(sync.stream());
+// }
+
+// exports.styles = styles;
 
 const styles = () => {
   return gulp.src("source/sass/style.scss")
@@ -14,10 +59,12 @@ const styles = () => {
     .pipe(sourcemap.init())
     .pipe(sass())
     .pipe(postcss([
-      autoprefixer()
+      autoprefixer(),
+      cssmin()
     ]))
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
